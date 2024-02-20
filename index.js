fef9d60e3b8ababa228be5b7d4d4d7db3ee92ce4
xodnd0507 <24457545yong@gmail.com>
2024-02-20 10:30:02 +0900

    chore : Next button add function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var 지금사진 = 1;
+
 $('.btn1').on('click', function(){
     $('.slide').css('transform', 'translateX(0vw');
 })
@@ -8,4 +10,17 @@ $('.btn2').on('click', function(){
 
 $('.btn3').on('click', function(){
     $('.slide').css('transform', 'translateX(-200vw');
+})
+
+$('.next').on('click', function(){
+    if (지금사진 == 1){
+        $('.slide').css('transform', 'translateX(-100vw');
+        지금사진 ++;
+    } else if(지금사진 == 2){
+        $('.slide').css('transform', 'translateX(-200vw');
+        지금사진 ++;
+    } else if(지금사진 == 3){
+        $('.slide').css('transform', 'translateX(0vw');
+        지금사진 = 1;
+    }
 })

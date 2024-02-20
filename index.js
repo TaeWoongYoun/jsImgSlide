28bcbfeedc867052b63ebac0502074d3c50352a6
xodnd0507 <24457545yong@gmail.com>
2024-02-20 09:42:58 +0900

    Test : button function update

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+$('.btn1').on('click', function(){
+    $('.slide').css('transform', 'translateX(0vw');
+})
+
 $('.btn2').on('click', function(){
     $('.slide').css('transform', 'translateX(-100vw');
+})
+
+$('.btn3').on('click', function(){
+    $('.slide').css('transform', 'translateX(-200vw');
 })

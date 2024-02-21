5ae96b191f776e67642818bab65099c649ecb67a
xodnd0507 <24457545yong@gmail.com>
2024-02-21 14:18:05 +0900

    chore : Add prev function

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ var 지금사진 = 1;
 
 $('.btn1').on('click', function(){
     $('.slide').css('transform', 'translateX(0vw');
+    지금사진 = 1;
 })
 
 $('.btn2').on('click', function(){
     $('.slide').css('transform', 'translateX(-100vw');
+    지금사진 = 2;
 })
 
 $('.btn3').on('click', function(){
     $('.slide').css('transform', 'translateX(-200vw');
+    지금사진 = 3;
 })
 
 $('.next').on('click', function(){
@@ -22,5 +25,18 @@ $('.next').on('click', function(){
     } else if(지금사진 == 3){
         $('.slide').css('transform', 'translateX(0vw');
         지금사진 = 1;
-    }
+    } 
+})
+
+$('.prev').on('click', function(){
+    if (지금사진 == 1){
+        $('.slide').css('transform', 'translateX(-200vw');
+        지금사진 =3;
+    } else if(지금사진 == 2){
+        $('.slide').css('transform', 'translateX(0vw');
+        지금사진 =1;
+    } else if(지금사진 == 3){
+        $('.slide').css('transform', 'translateX(-100vw');
+        지금사진 = 2;
+    } 
 })
